364f932f75cbdfb45e4a35ed699b060fb30fe16e
Sydney P <108637209+maleficsydney@users.noreply.github.com>
2023-03-30 17:29:18 -0500

    added most recent files
    updated files as a baseline for future project work on the site, all work here will be added from cloning and pull requests via VS Code

---
+++ javascript.js
@@ -0,0 +1,61 @@
+/* 
+Comment
+Variables
+Functions
+*/
+
+/* Create a contact form on the header that people can submit */
+
+const sydney = {
+    name: 'Sydney Pipkin',
+    age: '25',
+    city: 'Nashville, Tennessee',
+    resume: 'link'
+}
+
+
+function printInfo({name, age, city, resume}) {
+    console.log(`Name: ${name} `);
+    console.log(`Age: ${age} `);
+    console.log(`City: ${city} `);
+    console.log(`Resume: ${resume} `);
+}
+
+printInfo(sydney);
+
+/*
+const variableOne = 5;
+const variableTwo = 2;
+
+
+function code1() {
+    alert("You hear a click and push, your first number.");
+    alert(`${multiply}`);
+  }
+*/
+
+/*
+
+const sydney = {
+    name: 'Sydney Pipkin',
+    age: '25',
+    city: 'Nashville, Tennessee',
+    resume: 'link'
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+*/
+
+/*
+function printInfo({name, age, city, resume}) {
+    alert(`Name: ${name} `);
+    console.log(`Age: ${age} `);
+    console.log(`City: ${city} `);
+    console.log(`Resume: ${resume} `);
+}
+
+
+button.addEventListener("click", printInfo(sydney));
+*/
